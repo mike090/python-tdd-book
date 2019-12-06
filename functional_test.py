abfadb29ca39bf369cadb1329280b599ddd5e851
mike090 <mike09@mail.ru>
2019-12-06 23:14:10 +0300

    Refactor FT

--- functional_test.py
+++ functional_test.py
@@ -5,11 +5,20 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
+	to_do_item_1 = 'Buy peacock feathers'
+	to_do_item_2 = 'Use peacock feathers to make a fly'
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
 	def tearDown(self):
 		self.browser.quit()
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 
 	def test_can_start_a_list_and_retrive_it_later(self):
 
@@ -28,31 +37,25 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(self.to_do_item_1)
 
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows],
-			f'New to-do item did\'nt appear in table. Contents were:\n{table.text}')
+		self.check_for_row_in_list_table(f'1: {self.to_do_item_1}')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(self.to_do_item_2)
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
 		# The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table(f'1: {self.to_do_item_1}')
+		self.check_for_row_in_list_table(f'2: {self.to_do_item_2}')
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some

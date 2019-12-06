346a8eb4c5a6fa010290a7a3b77fe29bdce6d644
mike090 <mike09@mail.ru>
2019-12-07 00:57:53 +0300

    Make FT an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	to_do_item_1 = 'Buy peacock feathers'
 	to_do_item_2 = 'Use peacock feathers to make a fly'
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		# Eduth has heard about a cool new online to-do app. She goes
 		# to check out its home
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title) #"Browser title was {0}".format(browser.title)

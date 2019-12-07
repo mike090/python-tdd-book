38bc5457abec273e8dc0d9e23e2666b16d8506b1
mike090 <mike09@mail.ru>
2019-12-07 08:03:36 +0300

    Removed "voodoo" sleeps

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -18,6 +21,17 @@ class NewVisitorTest(LiveServerTestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
+
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				self.check_for_row_in_list_table(row_text)
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)	
 
 
 	def test_can_start_a_list_and_retrive_it_later(self):
@@ -42,20 +56,18 @@ class NewVisitorTest(LiveServerTestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
-		self.check_for_row_in_list_table(f'1: {self.to_do_item_1}')
+		self.wait_for_row_in_list_table(f'1: {self.to_do_item_1}')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys(self.to_do_item_2)
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
 		# The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table(f'1: {self.to_do_item_1}')
-		self.check_for_row_in_list_table(f'2: {self.to_do_item_2}')
+		self.wait_for_row_in_list_table(f'1: {self.to_do_item_1}')
+		self.wait_for_row_in_list_table(f'2: {self.to_do_item_2}')
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
@@ -64,6 +76,3 @@ class NewVisitorTest(LiveServerTestCase):
 		# She visits that URL - her to-do list is still there.
 		self.fail('Finish the test!')
 		# Satisfied, she goes back to sleep
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')

cb4573a424895cc700fb509b73a18b54dbfd66d0
mike090 <mike09@mail.ru>
2019-12-08 17:25:12 +0300

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,14 +8,24 @@ MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
+	item_display_format = '{0}: {1}'
 	to_do_item_1 = 'Buy peacock feathers'
 	to_do_item_2 = 'Use peacock feathers to make a fly'
+	to_do_item_3 = 'Buy milk'
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
 	def tearDown(self):
 		self.browser.quit()
+
+	def format_item(self, position, text):
+		return self.item_display_format.format(position, text)
+
+	def add_item(self, item_text):
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys(item_text)
+		inputbox.send_keys(Keys.ENTER)
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
@@ -51,23 +61,20 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
-		inputbox.send_keys(self.to_do_item_1)
+		self.add_item(self.to_do_item_1)
 
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
-		inputbox.send_keys(Keys.ENTER)
 
-		self.wait_for_row_in_list_table(f'1: {self.to_do_item_1}')
+		self.wait_for_row_in_list_table(self.format_item(1, self.to_do_item_1))
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys(self.to_do_item_2)
-		inputbox.send_keys(Keys.ENTER)
+		self.add_item(self.to_do_item_2)
 
 		# The page updates again, and now shows both items on her list
-		self.wait_for_row_in_list_table(f'1: {self.to_do_item_1}')
-		self.wait_for_row_in_list_table(f'2: {self.to_do_item_2}')
+		self.wait_for_row_in_list_table(self.format_item(1, self.to_do_item_1))
+		self.wait_for_row_in_list_table(self.format_item(2, self.to_do_item_2))
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
@@ -76,3 +83,44 @@ class NewVisitorTest(LiveServerTestCase):
 		# She visits that URL - her to-do list is still there.
 		self.fail('Finish the test!')
 		# Satisfied, she goes back to sleep
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		# Edith starts a new to-do list
+		self.browser.get(self.live_server_url)
+		self.add_item(self.to_do_item_1)
+		self.wait_for_row_in_list_table(self.format_item(1, self.to_do_item_1))
+
+		# She noticed that her list has a unique URL
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+
+		# Now a new user, Francis, comes along to the site.
+
+		## We use a new browser session to make sure that no information
+		## of Edith's is coming through from cookies etc
+
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# Francis visits the home page. There is no sign of Edith's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn(self.to_do_item_1, page_text)
+		self.assertNotIn(self.to_do_item_2, page_text)
+
+		# Francis starts a new list by entering a new item.
+		# He is less interesting then Edith...
+		self.add_item(self.to_do_item_3)
+		self.wait_for_row_in_list_table(self.format_item(1, self.to_do_item_3))
+
+		# Francis gets his unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEquals(francis_list_url, edith_list_url)
+
+		# Again, there is no trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn(self.to_do_item_1, page_text)
+		self.assertIn(self.to_do_item_3, page_text)
+
+		# Satisfied, they both go back to sleep
